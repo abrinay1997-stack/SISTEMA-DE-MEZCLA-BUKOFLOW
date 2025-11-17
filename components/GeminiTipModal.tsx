296fad92009c45d3231763fa2cf746e9577623f2
Abrinay <abrinay1997@gmail.com>
2025-11-17 12:13:34 -0500

    Delete components directory

--- components/GeminiTipModal.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// La funcionalidad de consejos de IA y este modal han sido deshabilitados.
-const GeminiTipModal: React.FC = () => {
-  return null;
-};
-
-export default GeminiTipModal;
